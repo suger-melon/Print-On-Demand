f3902f36a60866e5f3dd8b35f8f1def9f8ebd4d3
naimish <nabu.silentinfotech@gmail.com>
2025-03-05 15:31:52 +0530

    "ci: product listing from item array"

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,11 +1,70 @@
 import React from "react";
 import './styles.scss'
 
+// page import
+import SaleProduct from "../saleProduct/SaleProduct";
+
+// impages import
+
+// flash sales
+import sale1 from "./../../assets/products/sale1.png"
+import sale2 from "./../../assets/products/sale2.png"
+import sale3 from "./../../assets/products/sale3.png"
+import sale4 from "./../../assets/products/sale4.png"
+
+const ProductList = [
+    {
+        id: 1,
+        image: sale1,
+        name: "HAVIT HV-G92 Gamepad",
+        price: 120,
+        originalPrice: 160,
+        discount: 40,
+        rating: 3.5,
+        reviews: 88
+    }, 
+    {
+        id: 2,
+        image: sale2,
+        name: "AK-900 Wired Keyboard",
+        price: 960,
+        originalPrice: 1160,
+        discount: 200,
+        rating: 4,
+        reviews: 75
+    },
+    {
+        id: 3,
+        image: sale3,
+        name: "IPS LCD Gaming Monitor",
+        price: 370,
+        originalPrice: 400,
+        discount: 30,
+        rating: 5,
+        reviews: 99
+    },
+    {
+        id: 4,
+        image: sale4,
+        name: "S-Series Comfort Chair ",
+        price: 375,
+        originalPrice: 400,
+        discount: 25,
+        rating: 4.5,
+        reviews: 99
+    }
+]
 
 const Product = () => {
     return(
         <>
-           
+            <div className="product-container">
+                {
+                    ProductList.map(product => (
+                        <SaleProduct key={product.id} product={product}  />
+                    ))
+                }
+            </div>
         </>
     )
 }
--- src/components/saleProduct/SaleProduct.jsx
+++ src/components/saleProduct/SaleProduct.jsx
@@ -9,17 +9,34 @@ import fullStar from './../../assets/full-star.svg'
 import halfStart from './../../assets/half-star.svg'
 import emptyStart from './../../assets/empty-star.svg'
 
-const SaleProduct = () => {
+const SaleProduct = ({ product }) => {
+    const { name, image, price, originalPrice, discount, rating, reviews } = product
+
+    const renderStar = (rating) => {
+        const star = []
+        const fullStars = Math.floor(rating)
+        const hasHalfStar = rating % 1 !== 0
+        const emptyStar = 5 - fullStars - (hasHalfStar? 1: 0)
+
+        for(let i=0; i<fullStars; i++) star.push(<img key={i} src={fullStar} alt="star" />)
+        if(hasHalfStar) star.push(<img key="half" src={halfStart} alt="star" />)
+        for(let i=0; i<emptyStar; i++) star.push(<img key={`empty-${i}`} src={emptyStart} alt="star" />)
+
+        console.log(name)
+
+        return star
+    }
+
     return(
         <>
             <div className="saleProduct">
                 <div className="saleProduct-image">
                     <div className="sproduct">
-                        <img src={sale1} alt="game controller" />
+                        <img src={image} alt="game controller" />
                     </div>
                     <div className="d-flex justify-content-between">
                         <div className="percentage">
-                            - 40%
+                            {discount}
                         </div>
                         <div className="image-icon-parent">
                             <div className="image-icon">
@@ -32,19 +49,17 @@ const SaleProduct = () => {
                     </div>
                 </div>
                 <div className="saleProduct-text">
-                    <h5 className="mt-2">HAVIT HV-G92 Gamepad</h5>
+                    <h5 className="mt-2">{name}</h5>
                     <p className="mb-2">
-                        <span className="actual-price">$120</span>
-                        <span className="text-secondary text-decoration-line-through">$160</span>
+                        <span className="actual-price">${price}</span>
+                        <span className="text-secondary text-decoration-line-through">${originalPrice}</span>
                     </p>
                     
                     <div>
-                        <img src={fullStar} alt="star" />
-                        <img src={fullStar} alt="star" />
-                        <img src={fullStar} alt="star" />
-                        <img src={halfStart} alt="star" />
-                        <img src={emptyStart} alt="star" />
-                        <span className="ms-2 text-secondary">(88)</span>
+                        {
+                            renderStar(rating)
+                        }
+                        <span className="ms-2 text-secondary">({reviews})</span>
                     </div>
                     
                 </div>
